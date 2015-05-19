4172ec7accabc120203f1f8a5e8aa638d2029fb4
Dinesh729 <thakur.dinesh729@gmail.com>
2015-05-19 12:55:49 +0530

    New Facebook Login

---
+++ inc/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'facebook.php';
+
+$facebook = new Facebook(array(
+	'appId' => get_option('fb_app_id'),
+	'secret' => get_option('fb_app_key')
+));
+
+if($facebook->getUser() == 0){
+	$loginUrl = $facebook->getLoginUrl();
+	echo "<a href = '$loginUrl'>";
+	echo "<img src='../png/fb_login.jpeg'>";
+	echo "</a>";
+}
+else{
+	
+	$api = $facebook->api('me');
+	echo "Hi " . $api[name];
+	
+	//echo "<br><a href ='logout_fb.php'>Logout</a>";
+	
+}
+
+?>
